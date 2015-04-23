765122d11ab8c788fefc28a4737a7e11ac052580
Danneza Godinez <danneza96@gmail.com>
2015-04-23 11:18:10 +0800

    Now you see

---
+++ input.php
@@ -0,0 +1,6 @@
+
+name&nbsp;<input type="text" name="ngan" /><br />
+age&nbsp;<input type="password" name="age" /><br />
+address&nbsp;<input type="password" name="address" /><br />
+<input type="submit" value="Submit This!"/>
+
---
+++ process.php
@@ -0,0 +1,14 @@
+<?php
+
+$name = $_GET['ngan'];
+$age = $_GET['age']*365;
+$address = $_GET['address'];
+
+echo "HEllo" echo $name."<br />";
+echo "Congratulations you are now echo" $age."<br />" 
+echo "days old" ;
+echo "we are neighbors in echo" $address."<br />";
+
+
+
+?>
